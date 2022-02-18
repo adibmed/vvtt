d493d83fdfcf71caf92c644a6bc6dc3496d476b7
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-18 20:56:59 +0100

    feat: add vue router

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,21 @@
-import { createRouter } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({})
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import('../pages/Blog.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 export default router
