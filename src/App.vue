1aef8b0ef960eb097efe8585219c7ba289e3de72
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-18 19:39:25 +0100

    fix: eslint prettier conflict

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="h-screen"></div>
+  <div class="h-screen" />
   <img alt="Vue logo" src="./assets/logo.png" />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import "./css/main.css";
+import './css/main.css'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
