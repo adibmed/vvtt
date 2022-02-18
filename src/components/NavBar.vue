eb3dbbf57cb2e346b79636184727df9795bd24b4
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-18 21:03:49 +0100

    chore: add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav
+    class="z-50 backdrop-filter backdrop-blur-md py-1 pt-4 dark:bg-c-gray dark:border-gray-800 px-2 sm:px-0"
+  >
+    <div class="flex items-center px-1 mx-auto max-w-7xl sm:px-3 lg:px-6 py-4">
+      <div class="flex-1">
+        <span class="text-lg font-semibold text-gray-700 dark:text-gray-200"
+          ><nuxt-link :to="{ name: 'index' }">Adibe Mohamed</nuxt-link></span
+        >
+      </div>
+      <div class="flex-1 flex items-center justify-end">
+        <div class="pl-4 text-gray-500 hover:text-green-400 text-lg dark:text-gray-300">
+          <nuxt-link :to="{ name: 'blog' }">blog</nuxt-link>
+        </div>
+        <SocialIcons />
+        <ThemeToggler />
+      </div>
+    </div>
+  </nav>
+</template>
